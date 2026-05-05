1d2e4b828df7a3341f7d0227aa618917c4ec1eff
Felipe Duan <felipeduan1@gmail.com>
2026-05-05 09:49:53 -0300

    feat: add environment variables support for API config
    - Load config from .env file
    - Add configurable timeouts and base URLs
    - Support for web base URL and timeout

--- api/utils/config.py
+++ api/utils/config.py
@@ -1,1 +1,9 @@
-BASE_URL = "https://petstore.swagger.io/v2"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_URL = os.getenv("API_BASE_URL", "https://petstore.swagger.io/v2")
+REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", "10"))
+WEB_BASE_URL = os.getenv("WEB_BASE_URL", "https://www.saucedemo.com/")
+WEB_TIMEOUT = int(os.getenv("WEB_TIMEOUT", "10"))
